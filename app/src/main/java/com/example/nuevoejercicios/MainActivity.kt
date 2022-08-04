ecd55479a1ae2c0ede1ddced8e9d47a67e6d1c81
Svuara <robertoencina@gmail.com>
2022-08-04 20:58:39 -0300

    readline

--- app/src/main/java/com/example/nuevoejercicios/MainActivity.kt
+++ app/src/main/java/com/example/nuevoejercicios/MainActivity.kt
@@ -26,5 +26,36 @@ fun main(parametro: Array<String>)    {
     val perimetro = lado * 4
     println("El perimetro del cuadrado es " + perimetro)
  //    println("El perimetro del cuadrado es $perimetro")
+    print("Ingrese el precio de un producto: ")
+    val precio = readLine()!!.toDouble()
+    print("Ingrese la cantidad de productos: ")
+    val cantidad = readLine()!!.toInt()
+    val total = (precio * cantidad)
+    println("El total a pagar es $total")
+
+    print("Ingrese un numero: ")
+    val numero1 = readLine()!!.toInt()
+    print("Ingrese un numero: ")
+    val numero2 = readLine()!!.toInt()
+    val resultado1 = (numero1 + numero2)
+    println("el resultado de la primer suma es $resultado1")
+
+    print("Ingrese un numero: ")
+    val numero3 = readLine()!!.toInt()
+    print("Ingrese un numero: ")
+    val numero4 = readLine()!!.toInt()
+
+    val resultado2 = (numero3 * numero4)
+    println("el resultado de la suma de la 2 suma es $resultado2")
+
+    val resultado3 = (numero1 + numero2 + numero3 + numero4)
+    println("el resultado de la suma de todos los numeros  es $resultado3")
+
+    val resultado4 =  (numero1 + numero2 + numero3 + numero4) / 4
+    println("el promedio de todos los numeros es  $resultado4")
+
+
+
+
 }
 
